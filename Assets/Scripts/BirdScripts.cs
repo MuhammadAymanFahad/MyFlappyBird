07fb7110915055fa4acf179bf2a6e0ba113566c8
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-10-31 21:54:58 +0700

    add jump

--- Assets/Scripts/BirdScripts.cs
+++ Assets/Scripts/BirdScripts.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class BirdScripts : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * 10;
+        }
         
     }
 }
